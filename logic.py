7f5383e96ef2af85b4b6252879c35d2b06d53453
Witold Tafel <witold.tafel@gmail.com>
2018-03-14 14:56:22 +0100

    added geting id for the new row in logic.py

--- logic.py
+++ logic.py
@@ -1,8 +1,12 @@
 '''Connection layer between the routes and the CSV handling layer.
 It should have functions which can be called from the routing layer,
 and they should call persistence layer functions.'''
-
+import persistence
+import util
 from operator import itemgetter
+
+def get_id(list_dict):
+        return len(list_dict)
 
 def answer_dict(question_id, answer):
     ['id','submisson_time','vote_number','question_id','message','image']
@@ -14,10 +18,9 @@ def answer_dict(question_id, answer):
         'message' : answer,
         'image' : ''        
     }
+    answer_dict['id'] = get_id(persistence.list_of_dict_from_file('Answer.csv',util.ANS_FIELDS))
 
     return answer_dict
-
-
 
 
 def sort_list_of_dicts_by_time(dict_list):
